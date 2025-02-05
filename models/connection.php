ababa3040e8d2f218f84eaba1c5a35430cdc23e2
Felipe Diaz <fel.di.rod@gmail.com>
2025-02-05 12:49:40 -0300

    conecion a pos-communities

--- models/connection.php
+++ models/connection.php
@@ -4,7 +4,7 @@ class Connection{
 
 	public function connect(){
 
-		$link = new PDO("mysql:host=localhost;port=3306;dbname=pos-english", "root", "");
+		$link = new PDO("mysql:host=localhost;port=3306;dbname=pos-communities", "root", "");
 
 		$link -> exec("set names utf8");
 
